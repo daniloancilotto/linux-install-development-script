76b97c7b22a819e950253b08c9f1119066973678
Danilo Ancilotto <daniloancilotto@gmail.com>
2022-07-26 12:12:35 -0300

    Updated mysql workbench app version

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,7 +4,7 @@ system_release="`lsb_release -sr`"
 system_architecture="`uname -m`"
 
 echo "LINUX DEVELOPMENT SCRIPT (UBUNTU)"
-echo "Version: 2022.7.15-1940"
+echo "Version: 2022.7.26-1220"
 echo "Author: Danilo Ancilotto"
 echo "System: $system"
 echo "Architecture: $system_architecture"
@@ -67,7 +67,6 @@ printLine "Update"
 sudo apt update
 
 printLine "Kernel"
-
 sudo apt-mark auto $(apt-mark showmanual | grep -E "^linux-([[:alpha:]]+-)+[[:digit:].]+-[^-]+(|-.+)$")
 
 swappiness="10"
@@ -166,7 +165,7 @@ printLine "MySQL Workbench"
 root_app_name="mysql-workbench"
 root_app_subdir="$root_app_dir/$root_app_name"
 root_app_cversion="`sudo cat "$root_app_subdir/version.txt"`"
-root_app_version="8.0.29"
+root_app_version="8.0.30"
 
 if [ "$root_app_cversion" != "$root_app_version" ]
 then
