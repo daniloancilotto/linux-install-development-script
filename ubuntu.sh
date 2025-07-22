cfcfc509835262d7abf84e1e981d0f65e7ca6946
Danilo Ancilotto <daniloancilotto@gmail.com>
2025-07-22 08:39:42 -0300

    Updated mysql workbench app version

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,7 +4,7 @@ system_release="`lsb_release -sr`"
 system_architecture="`uname -m`"
 
 echo "INSTALL DEVELOPMENT APPS (UBUNTU)"
-echo "Version: 2025.04.21-1640"
+echo "Version: 2025.07.22-0840"
 echo "Author: Danilo Ancilotto"
 echo "System: $system"
 echo "Architecture: $system_architecture"
@@ -180,7 +180,7 @@ printLine "MySQL Workbench"
 root_app_name="mysql-workbench"
 root_app_subdir="$root_app_dir/$root_app_name"
 root_app_cversion="`sudo cat "$root_app_subdir/version.txt"`"
-root_app_version="8.0.42"
+root_app_version="8.0.43"
 
 if [ "$root_app_cversion" != "$root_app_version" ]
 then
